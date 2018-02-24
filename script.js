51ca003dc99568f581d1f7a65f5529b87a4312e7
Afnan Bintory56 <bintory56@gmail.com>
2018-02-23 21:39:22 -0800

    java scripting

---
+++ script.js
@@ -0,0 +1,186 @@
+
+
+
+
+
+
+     //  TASK#01
+alert("Error ! Please enter a valid password");
+
+
+
+       //TASK #02
+
+alert("Welcome to JS Alert \n  Happy Coding!");
+
+
+//       TASK#04
+
+console.log("Hello World");
+
+  //     Task#05
+   var myName="afnan";
+   var message="hello world";
+   alert(""+message+""+myName);
+
+    //   Task#06
+  var biodata1="Afnan Bintory";
+  var biodata2="Age=21";
+  alert(""+biodata1+""+boidata2);
+
+      // Task#07
+
+alert("pizza\npizz\npiz\npi\np");
+
+
+//       Task#08
+
+var e=email;
+var email1 ="abc@gmail.com";
+alert(""+e+""+email1);
+
+
+  //     Task#09
+
+document.write("Yay! i can write the js code very efficiently");
+
+var age="21";
+alert("my age"+age);
+
+    //      Task#10
+
+var a=0;
+alert(a++);
+
+
+      //    Task#11
+
+var birthyear="june";
+alert("my birth year\t"+birthyear);
+
+
+        //  Task#12
+
+var spec1=prompt("Visitor's Name");
+var spec2=prompt("Product Tiltle");
+var spec3=+prompt("Quantity");
+alert(""+spec1+"\t"+spec2+"\t"
++spec3);
+
+          //Task#13
+
+ var value=3;
+ alert("Initial value is \n"+value);
+ value++;
+ alert("After Increment \n"+value);
+
+ var value=8;
+ alert("Initial value is \n"+value);
+ value--;
+ alert("After decrement \n"+value);
+
+
+           //Task#14
+var tick=600;
+var p=tick*5;
+alert("Price for 5 person's is"+p);
+
+           //Task#15
+
+ var cel=18;
+ var far=56;
+ var c=(far-32) * 5/9;
+alert("Tempertaure in celsius"+c);
+ var f=(cel*9/5)+32;
+ alert("Tempertaure in farhenheit"+f);
+
+
+          // Task#16
+
+var tem=prompt("Temperature in degree");
+var c=(tem-32) * 5/9;
+alert("Temperature in Celsius"+c);
+var tem2=prompt("Temperature in fahrenheit");
+var f=(tem2-32) * 5/9;
+alert("Temperature in Celsius"+f);
+
+
+
+           //Task#17
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
